54f9cc7ac17148a9aae2adf19dfb12e12b41b58c
LuisFellipeT18 <luisfellipe.silva@outlook.com>
2024-03-06 10:41:23 -0500

    atualizacao

--- src/data/tools.tsx
+++ src/data/tools.tsx
@@ -2,8 +2,8 @@ export const tools = [
   {id: 1, name: "HTML", img: "/html.svg" },
   {id: 2, name: "CSS", img: "/css.svg" },
   {id: 3, name: "Javascript", img: "/js.svg" },
-  {id: 4, name: "Typescript", img:"/typescript2.svg" },
+  {id: 4, name: "Typescript", img:"/typescript2-white.png" },
   {id: 5, name: "Node", img: "/node.svg" },
-  {id: 6, name: "React", img: "/react.svg" },
+  {id: 6, name: "React", img: "/react-white.png" },
   {id: 7, name: "Angular", img: "/icone-angulaire-grise.png" },
 ];

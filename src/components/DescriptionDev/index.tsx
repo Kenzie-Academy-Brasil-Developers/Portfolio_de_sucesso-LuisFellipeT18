587114844e768ac3e631eb00df8506c2c308f547
LuisFellipeT18 <luisfellipe.silva@outlook.com>
2024-03-06 07:56:56 -0500

    finalizado

--- src/components/DescriptionDev/index.tsx
+++ src/components/DescriptionDev/index.tsx
@@ -8,7 +8,7 @@ export const DescritionDev = () => {
                     <h1 className={style.title_1}>Criando experiências por meio da tecnologia </h1>
                     <p className={style.paragraph}>Olá, sou estudante de programação na Kenzie Academy Brasil,
                     participei de diversos projetos resolvendo problemas de alto nível e desenvolvendo habilidades, um exemplo é o projeto "Desafio Back-end", feito desde o zero, 
-                    logotipo e aplicação e utilizando React e Typescript. Tenho formação imcompleta de designer gráfico (2 anos), onde aprendi sobre a criação de marcas logotipos,
+                    logotipo e aplicação e utilizando React e Typescript. Tenho formação incompleta de designer gráfico (2 anos), onde aprendi sobre a criação de marcas logotipos,
                     tipografia , história da arte, desenho, e realizei projetos publicitários, o que considero importante que me torna capaz de criar interfaces de usuário intuitivas
                     e atraentes. Sou casado há 4 anos, moro na Colômbia a 3 anos, meus hobbies são, realizar projetos pessoais no computador, tocar violão, videogame e jogar futebol. 
                     </p>

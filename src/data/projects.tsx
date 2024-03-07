d361b7be2bab37b975f97e271bb2dd22339cec9e
LuisFellipeT18 <luisfellipe.silva@outlook.com>
2024-03-07 13:09:05 -0500

    atualizacao

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -5,7 +5,7 @@ export const projects: Project[] = [
       id: 1,
       name: "Desafio Back-end",
       languages: ["HTML", "SCSS", "React", "Typescript"],  
-      description: "Esse é um projeto de finalização de curso, baseado em uma agenda virtual, onde é possível armazenar contatos. Possível criar seu usuário e sua rede de contatos com todas funcionalidades de edição, exclusão e atualização.",
+      description: "Projeto de finalização de curso, Back-end e Front-end, baseado em uma agenda virtual, onde é possível armazenar contatos. Possível criar seu usuário e sua rede de contatos com todas funcionalidades de edição, exclusão e atualização.",
       link_project: "https://desafio-beck-end.vercel.app/",
       github: "https://github.com/Kenzie-Academy-Brasil-Developers/M6-Desafio-BackEnd-LuisFellipeT18"
     },
@@ -13,7 +13,7 @@ export const projects: Project[] = [
       id: 2,
       name: "Burguer Kenzie",
       languages: ["HTML", "SCSS", "Javascript", "figma"], 
-      description: "Acessar uma lista de productos utilizando um database , simulando um carrinho de compras com React e utilizando como base um projeto estilizado no figma.",
+      description: "Acessa uma lista de productos utilizando um database , simulando um carrinho de compras com React e utilizando como base um projeto estilizado no figma.",
       link_project: "https://template-hamburgueria-tawny.vercel.app/",
       github: "https://github.com/Kenzie-Academy-Brasil-Developers/Template-Hamburgueria-LuisFellipe-T18"
     },
@@ -21,7 +21,7 @@ export const projects: Project[] = [
       id: 3,
       name: "Kenzie Hub",
       languages: ["HTML", "CSS", "Javascript", "React", "figma"], 
-      description: "Criar um projeto para o gerenciamento de tecnologias do usuário, utilizando style de referência no figma e uma API rodada localmente.",
+      description: "Um projeto para o gerenciamento de tecnologias aprendidas pelos alunos, utilizando style de referência no figma e uma API rodada localmente.",
       link_project: "https://kenzie-hub-eight-neon.vercel.app/",
       github: "https://github.com/Kenzie-Academy-Brasil-Developers/Entrega-Kenzie-hub-Luisfellipe-T18"
     },
@@ -29,7 +29,7 @@ export const projects: Project[] = [
       id: 4,
       name: "Portfólio Template",
       languages: ["HTML", "CSS", "Javascript", "React", "figma"], 
-      description: "Renderiza uma lista de tecnologias e projetos.",
+      description: "Se trata de um projeto que renderiza uma lista de tecnologias e projetos.",
       link_project: "https://portifolio-template-lyart.vercel.app/",
       github: "https://github.com/Kenzie-Academy-Brasil-Developers/m3-s1-entrega-portifolio-template-LuisFellipeT18"
     },
@@ -45,7 +45,7 @@ export const projects: Project[] = [
       id: 6,
       name: "Pet info ",
       languages: ["HTML", "CSS", "Javascript"], 
-      description: "Uma aplicação frontend, um formato de Blog sobre cuidados gerais com animais domésticos, com dicas e curiosidades. A aplicacão possui apenas duas páginas onde temos o logim e o cadastro.",
+      description: "Projeto frontend, em formato de Blog sobre cuidados gerais com animais domésticos, com dicas e curiosidades. A aplicacão possui apenas duas páginas onde temos o logim e o cadastro.",
       link_project: "https://petinfobase-luis-fellipe-t18.vercel.app/",
       github: "https://github.com/Kenzie-Academy-Brasil-Developers/petinfobase-LuisFellipe-T18"
     },
